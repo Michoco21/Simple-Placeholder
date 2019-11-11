c2768dd38b9c213f6a1571364f41c817441626ff
Michoco <michelleattlan@hotmail.com>
2019-11-11 21:04:02 +0100

    ajout de 3 fichiers

---
+++ commandes_vi.php
@@ -0,0 +1,98 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <h1>Commandes VI</h1>
+ <div class="ligne1">
+   <h2>Déplacement dans un fichier</h2>
+    <h3>Déplacement à l'aide des flèches</h3>
+    <ul>
+      <li>La touche h pour vous déplacer vers la gauche.</li>
+      <li>La touche l pour vous déplacer vers la droite.</li>
+      <li>La touche j pour vous déplacer vers le bas.</li>
+      <li>La touche k pour vous déplacer vers le haut.</li>
+    </ul>
+
+    <h3>Déplacement mot par mot</h3>
+    <ul>
+      <li>Appuyez sur w ("word") pour déplacer le curseur mot par mot vers la droite.</li>
+      <li>Appuyez sur b ("back") pour déplacer le curseur mot par mot vers la gauche.</li>
+      <li>Appuyez sur W ou sur B pour déplacer le curseur du signe de ponctuation le plus proche au blanc suivant ou précédent.</li>
+      <li>Appuyez sur e ("end") pour positionner le curseur sur le dernier caractère du mot en cours.</li>
+    </ul>
+
+    <h3>Déplacement vers le début ou la fin d'une ligne</h3>
+    <ul>
+      <li>Appuyez sur ^ pour amener le curseur au début de la ligne en cours.</li>
+      <li>Appuyez sur $ pour amener le curseur à la fin de la ligne en cours.</li>
+    </ul>
+
+    <h3>Déplacement ligne par ligne</h3>
+    <ul>
+      <li>Appuyez sur la touche Return pour positionner le curseur au début de la ligne suivante.</li>
+    </ul>
+
+    <h3>Déplacement vers la gauche</h3>
+    <ul>
+      <li>Appuyez sur la barre d'espacement pour déplacer le curseur d'un caractère vers la droite.</li>
+    </ul>
+
+    <h3>Déplacement vers la droite</h3>
+    <ul>
+      <li>Appuyez sur la barre d'espacement pour déplacer le curseur d'un caractère vers la droite.</li>
+    </ul>
+
+    <h3>Déplacement vers le haut</h3>
+    <ul>
+      <li>Appuyez sur H ("high") pour amener le curseur en haut de l'écran.</li>
+    </ul>
+
+    <h3>Déplacement vers le centre</h3>
+    <ul>
+      <li>Appuyez sur M ("middle") pour amener le curseur au centre de l'écran.</li>
+    </ul>
+
+    <h3>Déplacement vers le bas</h3>
+    <ul>
+      <li>Appuyez sur L ("low") pour amener le curseur en bas de l'écran.</li>
+    </ul>
+  </div>
+
+  <div class="ligne2">
+    <h2>Pagination et défilement</h2>
+    <h3>Défilement d'une page-écran</h3>
+    <p>Pour faire défiler une page-écran vers le bas, appuyez sur Ctrl-F ("Forward"). (Maintenez la touche Control enfoncée et appuyez sur la touche F.) Le curseur se positionne dans le coin supérieur gauche du nouvel écran.</p>
+
+    <h3>Défilement d'un demi-écran vers l'avant</h3>
+    <p>Pour faire défiler un demi-écran vers le bas, appuyez sur Ctrl-D ("Down").</p>
+
+    <h3>Défilement d'un écran vers l'arrière</h3>
+    <p>Pour faire défiler en arrière (vers le haut) une page-écran, appuyez sur Ctrl-B.</p>
+
+    <h3>Défilement d'un demi-écran vers l'arrière</h3>
+    <p>Pour faire défiler en arrière un demi-écran, appuyez sur Ctrl-U.</p>
+  </div>
+
+  <div class="ligne3">
+    <h2>Insertion de texte</h2>
+    <h3>Ajout</h3>
+    <ul>
+      <li>La commande a (ajout) permet d'insérer du texte à la droite du curseur. Positionnez le curseur sur une ligne, tapez a, puis le texte à ajouter. Appuyez sur Esc lorsque vous avez terminé.</li>
+      <li>La commande A permet d'insérer du texte à la fin d'une ligne. Positionnez le curseur sur une ligne de texte, puis tapez A. Le curseur est renvoyé à la fin de la ligne, où vous pouvez insérer votre texte. Appuyez sur Esc lorsque vous avez terminé.</li>
+    </ul>
+    <h3>Insertion</h3>
+    <ul>
+      <li>Vous pouvez insérer du texte à la gauche du curseur en tapant i en mode commande.</li>
+      <li>La commande I permet d'insérer du texte en début de ligne. (Elle ramène le curseur en début de ligne, quelle que soit sa position sur la ligne.) Comme pour toutes les commandes décrites dans cette section, vous devez appuyer sur Esc pour repasser en mode commande après avoir entré le texte à insérer.</li>
+    </ul>
+    <h3>Ouverture de lignes</h3>
+    <ul>
+      <li>Tapez o pour ouvrir une ligne au-dessous de la position courante du curseur, puis insérez, au besoin, quelques lignes de texte. Appuyez sur Esc lorsque vous avez terminé.</li>
+      <li>Tapez o pour ouvrir une ligne au-dessous de la position courante du curseur, puis insérez, au besoin, quelques lignes de texte. Appuyez sur Esc lorsque vous avez terminé.</li>
+    </ul>
+  </div>
+
+  <div class="ligne4">
+    Modification en cours !
+  </div>
